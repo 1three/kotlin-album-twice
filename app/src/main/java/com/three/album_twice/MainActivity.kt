bae65079f783e2dd5c890c048263fff0034099f5
1three <1_three@naver.com>
2023-11-10 13:36:48 +0900

    feat: 안드로이드 뒤로가기 버튼 이벤트 추가

--- app/src/main/java/com/three/album_twice/MainActivity.kt
+++ app/src/main/java/com/three/album_twice/MainActivity.kt
@@ -3,9 +3,13 @@ package com.three.album_twice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var isDouble = false
     override fun onCreate(savedInstanceState: Bundle?) {
         // 화면 보이기
         super.onCreate(savedInstanceState)
@@ -23,5 +27,23 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+    }
+
+    // 뒤로 가기 버튼 이벤트
+    override fun onBackPressed() {
+        Toast.makeText(this, "뒤로 가기를 두 번 누르면 앱이 종료됩니다.", Toast.LENGTH_LONG).show()
+
+        // 더블 클릭 시, 앱 종료
+        if (isDouble == true) {
+            finish()
+        }
+
+        isDouble = true
+
+        // 2초 후에 isDouble 변수를 false로 설정 (다시 더블 클릭하여 종료하도록 구성)
+        Handler().postDelayed({
+            isDouble = false
+        }, 2000)
+
     }
 }
